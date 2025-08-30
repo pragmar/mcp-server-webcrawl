6becc511737f9395ad7e145a4b54f331e2c8770a
pragmar <ben@pragmar.com>
2025-08-30 15:32:59 -0400

    update cli/help to include HTTrack and ArchiveBox

--- src/mcp_server_webcrawl/utils/cli.py
+++ src/mcp_server_webcrawl/utils/cli.py
@@ -21,8 +21,10 @@ Options:
   -h, --help     Show this help message and exit
 
 Where is my DATASRC?
-  wget           Directory containing the webroot archives
   warc           Directory containing WARC files
+  wget           Directory containing the webroot archives
+  archivebox     Directory above one or more archivebox init'ed dirs
+  httrack        Projects directory (~/websites/, /My Websites/)
   interrobot     Path to */interrobot.v2.db
   katana         Directory containing the webroot archives
   siteone        Directory containing the webroot archives
